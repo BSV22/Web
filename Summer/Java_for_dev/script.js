d9bf860460174658b560c5dece828c76aa5df3d4
Basavakiran Shivanand Deshmukh <basavakirandeshmukh@gmail.com>
2025-05-27 01:32:47 +0530

    Till Strings

--- Summer/Java_for_dev/script.js
+++ Summer/Java_for_dev/script.js
@@ -1,7 +1,32 @@
-console.log("Hey haw Are you?")
+console.log("Hey how Are you?")
 
 var a=10;
 var b=3;
 var c="HI"
+var myname = "Basavakiran"
+var bro = "Sharanabasava"
 console.log(a+b+7)
 console.log(typeof a, typeof b, typeof c)
+
+
+// STRINGS
+console.log(`Hi \nI am ${myname} and my brothers name is ${bro}`)
+// myname.bold()    // equals = '<b>Basavakiran</b>'
+// myname.blink('a')    // '<blink>Basavakiran</blink>'
+// myname.toUpperCase()
+// console.log(myname.toUpperCase())
+// console.log(myname.toLowerCase())
+// console.log(myname.length)
+// console.log(myname.slice(1,5))
+// console.log(myname.slice(6))
+// console.log(myname.replace('B', 'K').slice(0,5))
+// console.log(myname.toLocaleLowerCase().replace('a', 'z'))
+// console.log(myname.concat(" Shivanand", ' Deshmukh'))
+// console.log(myname+" Shivanand"+ ' Deshmukh')
+// console.log(("   Shivanand"+ ' Deshmukh').trim())
+// // string i immutable
+// console.log(myname)
+// console.log(myname[0])
+// console.log(myname.charAt(0))
+// console.log(myname.charAt(110))
+// console.log(myname.startsWith('B'))
